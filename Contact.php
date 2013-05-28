a556d5e94d39a9afa07dbcaae8711347dc90fe99
Oliver Winther <o.winther@gmail.com>
2013-05-28 17:07:05 +0200

    refact

--- Contact.php
+++ Contact.php
@@ -29,10 +29,15 @@
 }
 </style>
 
-    <iframe width="100%" height="350px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.dk/maps?q=8270+h%C3%B8jbjerg+google+maps&amp;ie=UTF8&amp;hq=&amp;hnear=H%C3%B8jbjerg&amp;gl=dk&amp;t=m&amp;ll=56.117807,10.205612&amp;spn=0.066989,0.145569&amp;z=12&amp;iwloc=near&amp;output=embed"></iframe><br /><small><a href="https://maps.google.dk/maps?q=8270+h%C3%B8jbjerg+google+maps&amp;ie=UTF8&amp;hq=&amp;hnear=H%C3%B8jbjerg&amp;gl=dk&amp;t=m&amp;ll=56.117807,10.205612&amp;spn=0.066989,0.145569&amp;z=12&amp;iwloc=A&amp;source=embed" style="color:#0000FF;text-align:left"></a></small>
-        <!-- Start kontakt Div 1 -->
+    <iframe width="100%" height="350px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.dk/maps?q=8270+h%C3%B8jbjerg+google+maps&amp;ie=UTF8&amp;hq=&amp;hnear=H%C3%B8jbjerg&amp;gl=dk&amp;t=m&amp;ll=56.117807,10.205612&amp;spn=0.066989,0.145569&amp;z=12&amp;iwloc=near&amp;output=embed"></iframe>
+    <br>
+    <small>
+        <a href="https://maps.google.dk/maps?q=8270+h%C3%B8jbjerg+google+maps&amp;ie=UTF8&amp;hq=&amp;hnear=H%C3%B8jbjerg&amp;gl=dk&amp;t=m&amp;ll=56.117807,10.205612&amp;spn=0.066989,0.145569&amp;z=12&amp;iwloc=A&amp;source=embed" style="color:#0000FF;text-align:left">
+        </a>
+    </small>
+            <!-- Start kontakt Div 1 -->
 	    <div class="row-fluid">
-    <!-- Start kontakt Div 1 Venstre -->
+            <!-- Start kontakt Div 1 Venstre -->
             <div class="span4">
                 <h4 align="center">Få fat på os.</h4>
                 <div class="continfo_item">
@@ -47,25 +52,33 @@
                     </span>
                     <b>30 74 77 40<br>86 27 77 40</b>
                 </div>
-                 <div class="continfo_item">
-                     <span class="ico_socialize">
-                         <img src="Img/mail.png" alt="Addressen" width="42" height="42">
-                     </span>
-                     <b>Skriv til os! <br> Vi forsøger at svare indenfor 24 timer</b>
-                 </div>
- 
-    </div>
-        <!-- Slut kontakt Div 1 Venstre -->
-    <div class="span4">
-        <h4 align="center">Kunde eller presse?</h4>
-        <br>
-        Uanset hvem du er så er vi altid klar på en snak. Har du fx spørgsmål omkring
-        <li>Levering</li>
-        <li>Presse</li>
-        <li>Reklamation</li>
-        Så tvivl ikke og grib telefonen
-    </div>
-    <div class="span4"><h4 align="center">Kunde eller presse?</h4><br>Uanset hvem du er så er vi altid klar på en snak. Har du fx spørgsmål omkring <li>Levering</li><li>Presse</li><li>Reklamation</li>Så tvivl ikke og grib telefonen</div>
+                <div class="continfo_item">
+                    <span class="ico_socialize">
+                        <img src="Img/mail.png" alt="Addressen" width="42" height="42">
+                    </span>
+                    <b>Skriv til os!<br>Vi forsøger at svare indenfor 24 timer</b>
+                </div>
+                
+            </div>
+                <!-- Slut kontakt Div 1 Venstre -->
+        <div class="span4">
+            <h4 align="center">Kunde eller presse?</h4>
+            <br>
+            Uanset hvem du er så er vi altid klar på en snak. Har du fx spørgsmål omkring
+            <li>Levering</li>
+            <li>Presse</li>
+            <li>Reklamation</li>
+            Så tvivl ikke og grib telefonen
+        </div>
+        <div class="span4">
+            <h4 align="center">Kunde eller presse?</h4>
+            <br>
+            Uanset hvem du er så er vi altid klar på en snak. Har du fx spørgsmål omkring
+            <li>Levering</li>
+            <li>Presse</li>
+            <li>Reklamation</li>
+            Så tvivl ikke og grib telefonen
+        </div>
     </div>
         <!-- Slut kontakt Div 1 -->
 
