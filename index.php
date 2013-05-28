7275ddb925eb649e85fb951950f1d563dbdabaa7
padydk <pady@live.dk>
2013-05-28 17:10:19 +0200

    index, calculator

--- index.php
+++ index.php
@@ -1,6 +1,27 @@
 <?php include 'Header.php'; ?>
 <?php include 'Header_left.php'; ?>
 <div id="maincontent">
+    <?php
+        // retrieve submitted information
+        $antalPersoner = $_POST['nrOfPersons'];
+        
+        // variables for database access
+        $dbUsername = "DB_015";
+        $dbPassword = "12399";
+
+        // query to retrieve information specific to submitted productcode
+        try {
+            $dbh = new PDO('oci:dbname=//localhost:1521/dbwc', $dbUsername, $dbPassword, array(PDO::ATTR_PERSISTENT => true));
+            $q = <<<SQL
+                SELECT *
+                FROM P_PRODUCTS
+                WHERE PRODUCTCODE = $code
+            SQL;
+                $stmt = $dbh->prepare($q);
+                $stmt->execute();
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }?>
     <div class="default_padding">
         <img src="Images/free-slider-img2.png" width="100%" alt="some_text">
     </div> 
@@ -13,9 +34,9 @@
         <form action="" method="POST" id="calculator">
             <label>Energibesparelse</label>
                 <select name="nrOfPersons" style="width: 20%">
-                    <option value="1">1 person</option>
-                    <option value="2">2 personer</option>
-                    <option value="3">3 personer</option>
+                    <option value="1"<? if (antalPersoner == 1) {echo 'selected="selected"'} ?>>1 person</option>
+                    <option value="2"<? if (antalPersoner == 2) {echo 'selected="selected"'} ?>>2 personer</option>
+                    <option value="3"<? if (antalPersoner == 3) {echo 'selected="selected"'} ?>>3 personer</option>
                 </select>
 		    <button type="submit">Beregn</button>
             <label>din besparelse er 0%</label>
