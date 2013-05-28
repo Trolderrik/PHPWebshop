8a571c0f13dcb8b0f88866aa481895ea2efb14ae
padydk <pady@live.dk>
2013-05-28 16:10:49 +0200

    major overhaul af projekt opsætning

--- header_left.php
+++ header_left.php
@@ -1,24 +1,25 @@
- <body>
-        <div id="page">
-            <header id="header_left">
-                <div class="navButton">
-                    <a href="http://divide.dk/1" target="_blank">
-                        <img src="img/textlogo.png" alt="Logo" />
-                    </a>
-                </div>
-                <div class="navButton">
-                    <a href="/Buy" target="_blank">
-                        <img src="img/buyButton.png" alt="Buy" />
-                    </a>
-                </div>
-                <div class="navButton">
-                    <a href="http://www.facebook.com/sharer.php?u=http://divide.dk" target="_blank">
-                        <img src="img/shareButton.png" alt="Del via facebook" />
-                    </a>
-                </div>
-                <div class="navButton">
-                    <a href="/Contact" target="_blank">
-                        <img src="img/contactButton.png" alt="About" />
-                    </a>
-                </div>
-            </header>   
+<body>
+    <div id="page">
+        <header id="header_left">
+            <div class="navButton">
+                <a href="/Index.php" target="_blank">
+                    <img src="Images/textlogo.png" alt="Logo" />
+                </a>
+            </div>
+            <div class="navButton">
+                <a href="/Buy.php" target="_blank">
+                    <img src="Images/buyButton.png" alt="Buy" />
+                </a>
+            </div>
+            <div class="navButton">
+                <a href="http://www.facebook.com/sharer.php?u=http://divide.dk" target="_blank">
+                    <img src="Images/shareButton.png" alt="Del via facebook" />
+                </a>
+            </div>
+            <div class="navButton">
+                <a href="/Contact.php" target="_blank">
+                    <img src="Images/contactButton.png" alt="About" />
+                </a>
+            </div>
+        </header>
+    </div>
